3fb50120388fb7f488eba2dc286151ce45cd1466
DexxterGWM <99261879+DexxterGWM@users.noreply.github.com>
2023-03-31 09:50:28 -0300

    main.py File

---
+++ 03 - [Interactive Coding Exercise]/main.py
@@ -0,0 +1,20 @@
+# Example Input
+
+'''
+What is your name? Angela
+'''
+
+# Example Output
+
+'''
+6
+'''
+
+# //
+
+# This code prints the number of characteres in a user's name.
+print( len( input(' What is your name? ') ) )
+
+# Notes: if input was "Jack"...
+#   1st: print(len('Jack'))
+#   2nd: print(4)
