8fee3e044eefc7b30b114777046541ab1653d4ef
DexxterGWM <99261879+DexxterGWM@users.noreply.github.com>
2023-03-31 09:50:07 -0300

    main.py File

---
+++ 02 - [Interactive Coding Exercise]/main.py
@@ -0,0 +1,31 @@
+# Example Output
+
+# When you run your program, it should print the following:
+'''
+Day 1 - String Manipulation
+String Concatenation is done with the "+" sign.
+e.g. print("Hello " + "world")
+New lines can be created with a backslash and n.
+'''
+
+# Fix the code bellow 👇🏻
+'''
+print(Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print(('New lines can be created with a backslash and n.')
+'''
+
+# //
+
+# 1. Missing double quotes before the word "Day".
+print(" Day 1 - String Manipulation")
+
+# 2. Outer double quotes changed to single quotes.
+print(' String Concatenation is done with the "+" sign.')
+
+# 3. Extra indentation removed
+print(' e.g. print("Hello " + "world")')
+
+# 4. Extra "(" in print function removed.
+print(' New lines can be created with a backslash and n.')
