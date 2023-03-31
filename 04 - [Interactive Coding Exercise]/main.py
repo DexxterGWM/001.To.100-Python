08dce1371c4ef7b36814e244094ac2226b466bdd
DexxterGWM <99261879+DexxterGWM@users.noreply.github.com>
2023-03-31 09:51:02 -0300

    main.py File

---
+++ 04 - [Interactive Coding Exercise]/main.py
@@ -0,0 +1,33 @@
+# Example Input
+
+'''
+a: 3
+b: 5
+'''
+
+# Example Output
+
+'''
+a: 5
+b: 3
+'''
+
+# //
+
+# 🚨 Don't change the code below 👇🏻
+a = input(' a: ')
+b = input(' b: ')
+# 🚨 Don't change the code above 👆🏻
+
+####################################
+# Write your code below this line 👇🏻
+c = a
+a = b
+b = c
+# Write your code above this line 👆🏻
+####################################
+
+# 🚨 Don't change the code below 👇🏻
+print(' a: ' + a)
+print(' b: ' + b)
+# 🚨 Don't change the code above 👆🏻
